367ba6b3a92ec6abd42fdffc0631ac3421a84428
SHOHEI-KOYAMA <s.koyama1011@gmail.com>
2017-07-07 18:12:23 +0900

    データベースにtable作成するためのmigrationファイル追加

---
+++ workspace/cms/database/migrations/2017_07_07_085048_item_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ItemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items_table', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('item_name');
+            $table->string('item_info');
+            $table->string('username');
+            $table->string('user_e_mail');
+            $table->datetime('regesterd_date');
+            $table->datetime('due_date');
+            $table->binary('item_file');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('items_table');
+    }
+}
